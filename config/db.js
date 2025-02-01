e36602dd02a620ea3134ef33a8087c8905762fe9
n3dusa <n3dusa00@students.oamk.fi>
2025-02-02 00:08:18 +0200

    commit

--- config/db.js
+++ config/db.js
@@ -1,21 +1,21 @@
-import pkg from 'pg'; // Import the CommonJS module
-const { Pool } = pkg; // Destructure the Pool class from the default export
+import pkg from 'pg'; 
+const { Pool } = pkg; 
 
-// Setup connection pool
+
 const pool = new Pool({
-  user: 'postgres',     // Replace with your PostgreSQL username
-  host: 'localhost',    // Replace with your database host
-  database: 'postgres', // Replace with your database name
-  password: '1305',     // Replace with your database password
-  port: 5432,           // Replace with your database port (default is 5432)
+  user: 'postgres',     
+  host: 'localhost',    
+  database: 'postgres', 
+  password: '1305',     
+  port: 5432,           
 });
 
-// Test the connection when starting
+
 async function testConnection() {
   try {
     const client = await pool.connect();
     console.log('Database connected successfully');
-    client.release();  // Release the client after testing
+    client.release();  
   } catch (err) {
     console.error('Error connecting to the database:', err.message);
   }
